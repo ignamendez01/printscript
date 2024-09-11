8ee3599a001998f2ddb4aab8da08e36393b014c4
ignamendez01 <ignacio.mendez@ing.austral.edu.ar>
2024-09-11 16:27:37 -0300

    Conditional interpreter checker

--- interpreter/src/main/java/interpreter/interpreters/ConditionalInterpreter.java
+++ interpreter/src/main/java/interpreter/interpreters/ConditionalInterpreter.java
@@ -8,6 +8,7 @@ import interpreter.Administrator;
 import interpreter.Interpreter;
 import interpreter.InterpreterFactory;
 import interpreter.Variable;
+import interpreter.response.ErrorResponse;
 import interpreter.response.InterpreterResponse;
 import interpreter.response.SuccessResponse;
 
@@ -18,25 +19,35 @@ public class ConditionalInterpreter implements InterpreterTypes<Conditional> {
     public InterpreterResponse interpret(Conditional astNode, Administrator administrator) throws Exception {
         Interpreter interpreter = InterpreterFactory.interpreterVersion(administrator,"1.1");
 
-        if(getValue(astNode.getOperator(), administrator)){
-            return  interpreter.interpretAST(astNode.getTrueBranch());
-        }else{
-            if (astNode.getFalseBranch() != null) {
-                return interpreter.interpretAST(astNode.getFalseBranch());
-            }else{
-                return new SuccessResponse("Conditional is false and there is no else");
+        try {
+            if (getValue(astNode.getOperator(), administrator)) {
+                return interpreter.interpretAST(astNode.getTrueBranch());
+            } else {
+                if (astNode.getFalseBranch() != null) {
+                    return interpreter.interpretAST(astNode.getFalseBranch());
+                } else {
+                    return new SuccessResponse("Conditional is false and there is no else");
+                }
             }
+        } catch (Exception e) {
+            return new ErrorResponse("conditional is not boolean type");
         }
     }
 
-    private boolean getValue(ValueNode value, Administrator administrator){
+    private boolean getValue(ValueNode value, Administrator administrator) throws Exception {
         switch (value){
             case BooleanOperator booleanOperator -> {
                 return booleanOperator.getValue();
             }
             case IdentifierOperator identifierOperator -> {
                 Variable v = administrator.getVariable(identifierOperator.getIdentifier());
-                return Objects.equals(administrator.getVariables().get(v), "true");
+                if (Objects.equals(administrator.getVariables().get(v), "true")) {
+                    return true;
+                }else if(Objects.equals(administrator.getVariables().get(v), "false")){
+                    return false;
+                }else{
+                    throw new Exception("identifier is not boolean type");
+                }
             }
             default -> throw new IllegalStateException("Unexpected value: " + value);
         }
